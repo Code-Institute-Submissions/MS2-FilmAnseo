f94ec58497abfea4595c9de27c79b15e5c98ca76
WorkingEdge <jclry76@gmail.com>
2021-01-13 20:15:20 +0000

    Add function to send form using emailJS

---
+++ js/sendEmail.js
@@ -0,0 +1,20 @@
+console.log("check");
+
+function sendMail(contactForm){
+    emailjs.send("service_xm6go8k","template_bk2ro5o",{
+from_name: contactForm.name.value,
+from_email: contactForm.email_address.value,
+add_film: contactForm.film_add_name.value,
+add_location: contactForm.film_add_location.value,
+add_message: contactForm.message_text.value,
+})
+    .then( 
+       function(response){
+           console.log("Successful", response);
+       },
+       function(error){
+          console.log("Failed", error);
+       });
+       return false;
+}
+
